ea13fe1331b34087e9de4d6e896e77247414c264
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-09-26 21:26:43 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const mongoose = require('mongoose')
 
+require('dotenv').config()
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-// GET ROUTE
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server! Im running"});
-    
+// DEPENDENCIES
+const methodOverride = require('method-override')
+
+// MIDDLEWARE
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+// ROUTE
+app.get("/", (req, res) => {
+    res.json({ message: "Your To Do List"});
   });
   
+// lists
+const listsController = require ('./controller/lists_controller.js')
+app.use('/lists', listsController)
+
 // PORT
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
