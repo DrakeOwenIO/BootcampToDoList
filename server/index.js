934ede164bd235cbaebcb2e6ea9c67c4cee3e5eb
Drake K. Owen <owendk@AutomatedResults.Biz>
2022-10-03 17:12:42 -0400

    Added some middleware for the database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose')
 
+// Requiring some parsing stuff
+var bodyParser = require('body-parser')
+
+// Require and config env file
 require('dotenv').config()
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,11 +14,13 @@ const methodOverride = require('method-override')
 
 // MIDDLEWARE
 app.use(methodOverride('_method'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true})) // to support URL-encoded bodies
+app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'js')
 app.engine('js', require('express-react-views').createEngine())
+
 
 // ROUTE
 app.get("/api", (req, res) => {
