40d7bb9d081736e95d5593274988ae95692a75d6
DrakeOwen <drake@reidfarms.com>
2022-10-05 02:24:52 -0300

    styled the test box and sumbit button

--- client/src/components/TodoInput.js
+++ client/src/components/TodoInput.js
@@ -1,4 +1,5 @@
 import React, {useRef} from 'react';
+import './ToDoInput.css'
 
 const TodoInput = ({ createToDo }) => {
     const todoInput = useRef('')
@@ -12,8 +13,8 @@ const TodoInput = ({ createToDo }) => {
 
     return(
         <form onSubmit={handleSubmit}>
-            <input type="text" ref={todoInput} required/>
-            <input type="submit"/>
+            <input type="text" ref={todoInput} required className='input'/>
+            <input type="submit" className='submitBtn'/>
         </form>
     )
 }
