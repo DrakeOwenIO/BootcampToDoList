4d4c3bff957a33a8d5736288be169d4e73245158
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-10-08 17:14:08 -0400

    save changes

---
+++ client/src/components/Form.js
@@ -0,0 +1,24 @@
+import React, {useRef} from 'react';
+import './ToDoInput.css'
+
+const form = document.forms[0];
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const formData = new FormData(this);
+  const entries = formData.entries();
+  const data = Object.fromEntries(entries);
+
+  // send out to a REST API
+  fetch("https://some.endpoint.dev", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(/**/)
+    .catch(/**/);
+});
+
+export default TodoInput;
